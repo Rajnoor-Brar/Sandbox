96071ff4dd54fdf59df43d5cb243f66c0fc4a52f
Rajnoor-Brar <rajnoorsbrar@outlook.in>
2024-10-20 22:33:32 +0530

    Update Newton_Forward.py

--- Python/Newton_Forward.py
+++ Python/Newton_Forward.py
@@ -5,16 +5,13 @@ h=1
 y=[1,4,8,16,32,64,128]
 n=len(y)
 
-del_y0 = [0 for _ in range(n)]
-a=del_y0.copy()
+del_y0 = [0 for _ in range(n)];     a=del_y0.copy()
 
 for i in range(n):
-    for j in range(i+1):
-        del_y0[i]+=((-1)**j)*y[i-j]*math.comb(i,j)
+    for j in range(i+1):    del_y0[i]+=((-1)**j)*y[i-j]*math.comb(i,j)
     a[i]=del_y0[i]/(h**i)/math.factorial(i)
         
 equation=0
-
 for i in range(n):
     roots=[x0+k*h for k in range(i)]
     coeffs=eq.rootCoeffs(roots=roots)
@@ -22,5 +19,4 @@ for i in range(n):
 
 equation.name='y'
 print(equation)
-
 for i in range(n): print(f'y({x0+i*h}) = {equation(x0+i*h)}')
